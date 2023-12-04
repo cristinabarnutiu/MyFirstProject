51287f6cfb9b6748045e02ce6ae684cc58bc2246
cristina <cristina.ilea2@yahoo.com>
2023-12-04 19:37:33 +0200

    update exercises JavaMethods 4 dec 2023

--- src/JavaMethods.java
+++ src/JavaMethods.java
@@ -19,7 +19,11 @@ public class JavaMethods {
         System.out.println(getProductWithReturn(5,6));
         System.out.println(areMere("Ana"));
         System.out.println(areMere("Diana"));
-
+        System.out.println(metodaEx6("abcdefg"));
+        System.out.println(metodaEx5Optionale(-5,7,1,87));
+        System.out.println(metodaEx6Optionale("Mesopotamia"));
+        System.out.println(metodaEx6Optionale("casa"));
+        System.out.println(metodaEx8Optionale(873));
     }
 
     private static void printHello(){
@@ -73,6 +77,56 @@ public class JavaMethods {
      private static String areMere(String nume){
         return nume+" are mere";
      }
+
+  //Scrieti o metoda care primeste un String si returneaza inversul lui (Ex. “abcd”->”dcba”)
+    private static String metodaEx6(String cuvant){
+        String cuvantInversat = "";
+        int lungime = cuvant.length();
+        for (int i = lungime-1; i>=0;i--){
+            cuvantInversat = cuvantInversat+cuvant.charAt(i);
+        }
+        return cuvantInversat;
+    }
+
+    //problema 5 exercitii optionale
+    private static int metodaEx5Optionale(int a, int b, int c, int d){
+        int max= a;
+        if (b>max){
+            max = b;
+        }
+        if (c>max){
+            max=c;
+        }
+        if(d>max){
+            max =d;
+        }
+
+        return max;
+    }
+
+    private static String metodaEx6Optionale(String cuvant){
+        String litereleDinMijloc="";
+        int lungimeaCuvantului = cuvant.length();
+        if (lungimeaCuvantului%2==0){ //daca avem un nr par de caractere
+            int pozitieLitera1=lungimeaCuvantului/2-1;
+            int pozitieLitera2=lungimeaCuvantului/2;
+            litereleDinMijloc=litereleDinMijloc+cuvant.charAt(pozitieLitera1)+cuvant.charAt(pozitieLitera2);
+        }
+        else litereleDinMijloc=litereleDinMijloc+cuvant.charAt(lungimeaCuvantului/2);
+
+        return litereleDinMijloc;
+    }
+
+    //ex 8 Optionale
+    //873
+    public static int metodaEx8Optionale(int numar){
+        int sum=0;
+        while(numar!=0) {
+            sum = sum + numar % 10;
+            numar = numar / 10;
+        }
+        return sum;
+    }
 
 
 
